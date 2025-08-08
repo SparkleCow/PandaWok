57e477b1cfdf012bbf4345a143441209c4f056a5
SparkleCow <cjelcazador200@outlook.com>
2025-08-07 21:42:39 -0500

    creating repository

---
+++ src/main/java/com/sparklecow/pandawok/PandawokApplication.java
@@ -0,0 +1,13 @@
+package com.sparklecow.pandawok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandawokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandawokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparklecow/pandawok/PandawokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparklecow.pandawok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandawokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
